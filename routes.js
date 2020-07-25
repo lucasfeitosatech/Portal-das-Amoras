c22f96db1652271ec0d347aba0f35653e1d38e0c
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2020-07-25 01:55:13 -0300

    [ADD] - Adicionando arquvo de rotas

---
+++ routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = new express.Router()
+// const indexController = require('./website/IndexController');
+// const easytrackingController = require('./easytracking/EasyTrackingController');
+// const filetrackingController = require('./filetracking/FileTrackingController');
+
+
+// module.exports = routes;
+
+// //Rotas para website
+// routes.get('/', indexController.index);
+
+// //Routes para EasyTrackingController
+// routes.get('/easytracking',easytrackingController.easytrackingRender);  
+// routes.post('/easytracking',easytrackingController.easytrackingPost);
+
+// //Routes para FileTrackingController
+// routes.get('/filetracking',filetrackingController.filetrackingRender);  
+// routes.post('/filetracking',filetrackingController.filetrackingPost);
+
