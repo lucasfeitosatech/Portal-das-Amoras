c7c90c11385642e978204bd8de2c4b1e930888dd
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2020-07-25 01:54:55 -0300

    [ADD - Configuracao inicial do server

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+//const qr = require('qr-image');
+const ejs = require('ejs');
+const app = express();
+// app.use("/website", express.static(__dirname + '/website'));
+
+app.set('view engine', 'ejs');
+// Set static folder
+// app.use(express.static('static'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//app.use(require('./routes'));
+
+
+
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+const path = require('path');
+
